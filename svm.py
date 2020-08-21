fdd1df409d291d4d74791fd7cdc633d75422e3d2
lennart <mail@lennartberning.de>
2020-08-21 17:38:49 +0200

    Added typehints and docstrings

--- svm.py
+++ svm.py
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
+import sklearn
 
 
+def main(input: str) -> str:
+    """
+    Returns a string.
+
+    Parameters:
+        input (str): The string which is the input.
+
+    Returns:
+        output (str): The string which gets returned.
+    """
+
+    output = input
+    return output
+
+
+print(main('test'))
